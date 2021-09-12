fffbb3e59ec21047236f7f71dc51705d1c609b30
lvapi <550368806@qq.com>
2021-09-12 19:04:24 +0800

    el-icon

---
+++ icon/src/icon.vue
@@ -0,0 +1,13 @@
+<template>
+  <i :class="'el-icon-' + name"></i>
+</template>
+
+<script>
+  export default {
+    name: 'ElIcon',
+    // 引入属性并绑定
+    props: {
+      name: String
+    }
+  };
+</script>
