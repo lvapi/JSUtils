d2f1b0be2e750b47d4501ec225d12655949fdc5b
lvapi <550368806@qq.com>
2021-09-25 17:01:15 +0800

    card

---
+++ card/src/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
+    <!-- 通过具名组件插槽或者属性的方式获得头部信息 -->
+    <div class="el-card__header" v-if="$slots.header || header">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <!-- 获得匿名组件插槽数据 -->
+    <div class="el-card__body" :style="bodyStyle">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ElCard',
+    props: {
+      header: {},
+      bodyStyle: {},
+      shadow: {
+        type: String
+      }
+    }
+  };
+</script>
