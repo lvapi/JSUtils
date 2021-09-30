3d02e2db71ea2d802c3344946f9eecf895b044bf
lvapi <550368806@qq.com>
2021-09-30 22:56:49 +0800

    el-radio

---
+++ radio/src/radio.vue
@@ -0,0 +1,143 @@
+<template>
+  <label
+    class="el-radio"
+    :class="[
+      border && radioSize ? 'el-radio--' + radioSize : '',
+      { 'is-disabled': isDisabled },
+      { 'is-focus': focus },
+      { 'is-bordered': border },
+      { 'is-checked': model === label }
+    ]"
+    role="radio"
+    :aria-checked="model === label"
+    :aria-disabled="isDisabled"
+    :tabindex="tabIndex"
+    @keydown.space.stop.prevent="model = isDisabled ? model : label"
+  >
+    <span class="el-radio__input"
+      :class="{
+        'is-disabled': isDisabled,
+        'is-checked': model === label
+      }"
+    >
+      <span class="el-radio__inner"></span>
+      <input
+        ref="radio"
+        class="el-radio__original"
+        :value="label"
+        type="radio"
+        aria-hidden="true"
+        v-model="model"
+        @focus="focus = true"
+        @blur="focus = false"
+        @change="handleChange"
+        :name="name"
+        :disabled="isDisabled"
+        tabindex="-1"
+        autocomplete="off"
+      >
+    </span>
+    <span class="el-radio__label" @keydown.stop>
+      <slot></slot>
+      <template v-if="!$slots.default">{{label}}</template>
+    </span>
+  </label>
+</template>
+<script>
+  import Emitter from 'element-ui/src/mixins/emitter';
+
+  export default {
+    name: 'ElRadio',
+
+    mixins: [Emitter],
+
+    inject: {
+      elForm: {
+        default: ''
+      },
+
+      elFormItem: {
+        default: ''
+      }
+    },
+
+    componentName: 'ElRadio',
+
+    props: {
+      value: {},
+      label: {},
+      disabled: Boolean,
+      name: String,
+      border: Boolean,
+      size: String
+    },
+
+    data() {
+      return {
+        focus: false
+      };
+    },
+    computed: {
+      //  通过获取当前节点的父节点判断是否为radio group
+      isGroup() {
+        // 获取当前节点的父节点
+        let parent = this.$parent;
+        while (parent) {
+          // 判断父节点的 自定义属性是否为 ElRadioGroup
+          if (parent.$options.componentName !== 'ElRadioGroup') {
+            parent = parent.$parent;
+          } else {
+            // 为redio-group
+            this._radioGroup = parent;
+            return true;
+          }
+        }
+        return false;
+      },
+      model: {
+        // 获取数据时触发 如果为radiogroup则绑定group的值否则为value的值
+        get() {
+          return this.isGroup ? this._radioGroup.value : this.value;
+        },
+        // 设置时触发
+        set(val) {
+          // 如果为group 使用异步操作向数组 veux中存储数据
+          if (this.isGroup) {
+            this.dispatch('ElRadioGroup', 'input', [val]);
+          } else {
+            // 否则触发父组件的input方法
+            this.$emit('input', val);
+          }
+          this.$refs.radio && (this.$refs.radio.checked = this.model === this.label);
+        }
+      },
+      // 通过数据注入的方式获取当前的表单的大小
+      _elFormItemSize() {
+        return (this.elFormItem || {}).elFormItemSize;
+      },
+      radioSize() {
+        const temRadioSize = this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+        return this.isGroup
+          ? this._radioGroup.radioGroupSize || temRadioSize
+          : temRadioSize;
+      },
+      isDisabled() {
+        return this.isGroup
+          ? this._radioGroup.disabled || this.disabled || (this.elForm || {}).disabled
+          : this.disabled || (this.elForm || {}).disabled;
+      },
+      tabIndex() {
+        return (this.isDisabled || (this.isGroup && this.model !== this.label)) ? -1 : 0;
+      }
+    },
+
+    methods: {
+      handleChange() {
+        this.$nextTick(() => {
+          this.$emit('change', this.model);
+          this.isGroup && this.dispatch('ElRadioGroup', 'handleChange', this.model);
+        });
+      }
+    }
+  };
+</script>
