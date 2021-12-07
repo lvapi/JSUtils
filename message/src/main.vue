66eae3e20420086c97f9ddbea47def69acb9e609
lvapi <550368806@qq.com>
2021-12-07 21:39:12 +0800

    el-message

---
+++ message/src/main.vue
@@ -0,0 +1,119 @@
+<template>
+  <transition name="el-message-fade" @after-leave="handleAfterLeave">
+    <div
+      :class="[
+        'el-message',
+        type && !iconClass ? `el-message--${ type }` : '',
+        center ? 'is-center' : '',
+        showClose ? 'is-closable' : '',
+        customClass
+      ]"
+      :style="positionStyle"
+      v-show="visible"
+      @mouseenter="clearTimer"
+      @mouseleave="startTimer"
+      role="alert">
+      <i :class="iconClass" v-if="iconClass"></i>
+      <i :class="typeClass" v-else></i>
+      <!-- <如果有组件插槽时优先显示组件插槽内容否则显示message属性的绑定值> -->
+      <slot>
+        <p v-if="!dangerouslyUseHTMLString" class="el-message__content">{{ message }}</p>
+        <p v-else v-html="message" class="el-message__content"></p>
+      </slot>
+      <i v-if="showClose" class="el-message__closeBtn el-icon-close" @click="close"></i>
+    </div>
+  </transition>
+</template>
+
+<script type="text/babel">
+  const typeMap = {
+    success: 'success',
+    info: 'info',
+    warning: 'warning',
+    error: 'error'
+  };
+
+  export default {
+    data() {
+      return {
+        visible: false,
+        message: '',
+        duration: 3000,
+        type: 'info',
+        iconClass: '',
+        customClass: '',
+        onClose: null,
+        showClose: false,
+        closed: false,
+        verticalOffset: 20,
+        timer: null,
+        dangerouslyUseHTMLString: false,
+        center: false
+      };
+    },
+
+    computed: {
+      typeClass() {
+        return this.type && !this.iconClass
+          ? `el-message__icon el-icon-${ typeMap[this.type] }`
+          : '';
+      },
+      positionStyle() {
+        return {
+          'top': `${ this.verticalOffset }px`
+        };
+      }
+    },
+    /*监听close值得变化如果为true时则隐藏提升框的显示*/
+    watch: {
+      closed(newVal) {
+        if (newVal) {
+          this.visible = false;
+        }
+      }
+    },
+
+    methods: {
+      handleAfterLeave() {
+        this.$destroy(true);
+        this.$el.parentNode.removeChild(this.$el);
+      },
+      /*调用关闭前的回调返回实例为当前message实例*/
+      close() {
+        this.closed = true;
+        if (typeof this.onClose === 'function') {
+          this.onClose(this);
+        }
+      },
+      /*当鼠标移入时清除计时器*/
+      clearTimer() {
+        clearTimeout(this.timer);
+      },
+      /*当鼠标离开时在延时duration毫秒后调用关闭*/
+      startTimer() {
+        if (this.duration > 0) {
+          this.timer = setTimeout(() => {
+            if (!this.closed) {
+              this.close();
+            }
+          }, this.duration);
+        }
+      },
+      // 触发键盘事件如果为esc时关闭弹窗
+      keydown(e) {
+        if (e.keyCode === 27) { // esc关闭消息
+          if (!this.closed) {
+            this.close();
+          }
+        }
+      }
+    },
+    mounted() {
+      this.startTimer();
+      document.addEventListener('keydown', this.keydown);
+    },
+    beforeDestroy() {
+      document.removeEventListener('keydown', this.keydown);
+    }
+  };
+</script>
